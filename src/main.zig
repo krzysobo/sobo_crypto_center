c70edb3806577d4dfa5c681087fbc9cc68672ec3
Krzysztof Sobolewski <krzysztof.sobolewski@gmail.com>
2025-05-02 00:54:48 +0200

    RE #app without capy

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,6 @@
 const std = @import("std");
-const capy = @import("capy");
-
-// This is required for your app to build to WebAssembly and other particular architectures
-pub usingnamespace capy.cross_platform;
 
 pub fn main() !void {
-    try capy.backend.init();
+    std.debug.print("\nHello World in Sobo-Crypto-Center.\n\n", .{});
 
-    var window = try capy.Window.init();
-    try window.set(
-        capy.label(.{ .text = "Hello, World", .alignment = .Center }),
-    );
-
-    window.setTitle("Hello");
-    window.setPreferredSize(250, 100);
-    window.show();
-
-    capy.runEventLoop();
 }
